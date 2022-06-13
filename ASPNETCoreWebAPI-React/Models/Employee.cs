6ceb028c3b1763f9bef865b90f75c9a259fc0a24
Fakhir Ahmad <fakhirahmad@hotmail.com>
2022-06-13 22:09:09 +0500

    Add project files.

---
+++ ASPNETCoreWebAPI-React/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreWebAPI_React.Models
+{
+    public class Employee
+    {
+        public long EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+}
